2754c3c3c763268e4b6c6ab6dd75f81a8c1d21b4
ShahdMasri <shahd.227.almasri@gmail.com>
2024-07-25 22:27:07 +0300

    feat1

---
+++ src/test/java/project/AcceptanceTest.java
@@ -0,0 +1,15 @@
+package project;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+@CucumberOptions ( features = "feature",
+        plugin = {"summary", "html:target/cucumber/report.html"},
+        monochrome=true,
+        snippets =SnippetType.CAMELCASE,
+        glue = {"project"})
+public class AcceptanceTest {
+}
+
