2b63e1ebc1ef0d5b9b4563218eb0e27572a2ad9b
SamiaAsmar <175207878+SamiaAsmar@users.noreply.github.com>
2024-08-23 19:14:28 +0300

    Update sweet.java

--- src/main/java/sweetSys/sweet.java
+++ src/main/java/sweetSys/sweet.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
+
 
 public class sweet {
     public static List<person> peopleList = new ArrayList<>();
