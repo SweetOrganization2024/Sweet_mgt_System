6d9a0b66e2455944fa886ec82acdd8778957af99
ShahdMasri <shahd.227.almasri@gmail.com>
2024-08-23 17:05:07 +0300

    Update DiscountManager.java

--- src/main/java/sweetSys/DiscountManager.java
+++ src/main/java/sweetSys/DiscountManager.java
@@ -1,17 +1,5 @@
 package sweetSys;
-import java.util.ArrayList;
 
-public class DiscountManager {
-     private String id;
-    private final String description;
-    private final double percentage;
-    private static final ArrayList<DiscountManager> discounts = new ArrayList<>();
-
-    public static ArrayList<DiscountManager> getDiscounts() {
-        return discounts;
-    }
-
-    public DiscountManager(String id, String description, double percentage) {package sweetSys;
 import java.util.ArrayList;
 
 public class DiscountManager {
@@ -20,7 +8,13 @@ public class DiscountManager {
     private final double percentage;
     private static final ArrayList<DiscountManager> discounts = new ArrayList<>();
 
-     public void setStart(String start) {
+    public DiscountManager(String id, String description, double percentage) {
+        this.id = id;
+        this.description = description;
+        this.percentage = percentage;
+    }
+
+    public void setStart(String start) {
         // Implementation for setting the start date
     }
 
@@ -32,16 +26,10 @@ public class DiscountManager {
         return discounts;
     }
 
-    public DiscountManager(String id, String description, double percentage) {
-        this.id = id;
-        this.description = description;
-        this.percentage = percentage;
-    }
     public static void addDiscount(DiscountManager discount) {
         discounts.add(discount);
     }
 
-
     public String getId() {
         return id;
     }
@@ -49,6 +37,7 @@ public class DiscountManager {
     public void setId(String id) {
         this.id = id;
     }
+
     public String getDescription() {
         return description;
     }
@@ -57,54 +46,14 @@ public class DiscountManager {
         return percentage;
     }
 
-    public static double applyDiscountToProduct(String id ,DiscountManager d) {
-        double dis=0.0;
+    public static double applyDiscountToProduct(String id, DiscountManager d) {
+        double dis = 0.0;
         double originalPrice;
-        double per =d.getPercentage();
-        for(newSweet n : sweet.getListOfSweet()){
-            if(n.getId_of_sweet().equals(id)){
-                originalPrice=Double.parseDouble(n.getPrice());
-                dis =originalPrice * (per / 100);
-                n.setPrice(String.format("%.2f", dis));
-                break;
-            }
-        }
-        return dis;
-    }
-}
-
-        this.id = id;
-        this.description = description;
-        this.percentage = percentage;
-    }
-    public static void addDiscount(DiscountManager discount) {
-    discounts.add(discount);
-    }
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getDescription() {
-        return description;
-    }
-
-    public double getPercentage() {
-        return percentage;
-    }
-
-    public static double applyDiscountToProduct(String id ,DiscountManager d) {
-        double dis=0.0;
-        double originalPrice;
-        double per =d.getPercentage();
-        for(newSweet n : sweet.getListOfSweet()){
-            if(n.getId_of_sweet().equals(id)){
-                originalPrice=Double.parseDouble(n.getPrice());
-                dis =originalPrice * (per / 100);
+        double per = d.getPercentage();
+        for (newSweet n : sweet.getListOfSweet()) {
+            if (n.getId_of_sweet().equals(id)) {
+                originalPrice = Double.parseDouble(n.getPrice());
+                dis = originalPrice * (per / 100);
                 n.setPrice(String.format("%.2f", dis));
                 break;
             }
