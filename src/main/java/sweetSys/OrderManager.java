4b39951a7a7f76fddf9babce9f8ce0ba0075cf7e
ShahdMasri <shahd.227.almasri@gmail.com>
2024-08-23 19:52:15 +0300

    Update OrderManager.java

--- src/main/java/sweetSys/OrderManager.java
+++ src/main/java/sweetSys/OrderManager.java
@@ -3,7 +3,6 @@ package sweetSys;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class OrderManager {
