fe64c2a9e930bc0661e541382029c624e384d03f
SamiaAsmar <175207878+SamiaAsmar@users.noreply.github.com>
2024-08-23 20:19:20 +0300

    Update EmailSender.java

--- src/main/java/sweetSys/EmailSender.java
+++ src/main/java/sweetSys/EmailSender.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 
 public class EmailSender {
     private static final String EMAIL_USERNAME = "SweetSystemInstitution@gmail.com";
-    private static final String EMAIL_PASSWORD = "dzfp eitg yfkg nspe";
+    private static final String password = System.getenv("password");
     private static final Logger logger = Logger.getLogger(EmailSender.class.getName());
 
     public EmailSender(){
