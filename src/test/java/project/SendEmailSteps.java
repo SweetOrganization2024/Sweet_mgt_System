3622c4348d6267bbb80a2bb51e84deffec9d1305
SamiaAsmar <175207878+SamiaAsmar@users.noreply.github.com>
2024-08-24 19:20:40 +0300

    Update SendEmailSteps.java

--- src/test/java/project/SendEmailSteps.java
+++ src/test/java/project/SendEmailSteps.java
@@ -1,18 +1,20 @@
 package project;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-
+import static org.junit.Assert.assertTrue;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import sweetSys.EmailSender;
+import java.util.logging.Logger;
+
 public class SendEmailSteps {
 
     private String toEmail;
     private String subject;
     private String body;
     private Exception exception;
+    private EmailSender emailSender;
+    private Logger logger = Logger.getLogger(SendEmailSteps.class.getName());
 
     @Given("a valid email address {string}")
     public void aValidEmailAddress(String email) {
@@ -44,22 +46,48 @@ public class SendEmailSteps {
         this.body = "";
     }
 
+    @Given("the email sending fails due to a messaging exception")
+    public void theEmailSendingFailsDueToMessagingException() {
+
+    }
+
+    @Given("an invalid email address {string}")
+    public void anInvalidEmailAddress(String email) {
+        this.toEmail = email;
+    }
+
     @When("the email is sent")
     public void theEmailIsSent() {
         try {
-            EmailSender.sendEmail(toEmail, subject, body);
+            if (emailSender == null) {
+                emailSender = new EmailSender(); // Initialize the real EmailSender if not mocked
+            }
+            emailSender.sendEmail(toEmail, subject, body);
+            exception = null;
         } catch (Exception e) {
-            this.exception = e;
+            exception = e;
+        }
+    }
+
+    @Then("an error should be thrown with message {string}")
+    public void anErrorShouldBeThrownWithMessage(String expectedMessage) {
+        assertTrue("An exception should have been thrown", exception != null);
+        assertEquals("Error message should match", expectedMessage, exception.getMessage());
+    }
+
+    @Then("an error should be logged with message {string}")
+    public void anErrorShouldBeLoggedWithMessage(String expectedLogMessage) {
+        try {
+            if (emailSender == null) {
+                emailSender = new EmailSender();
+            }
+            emailSender.sendEmail(toEmail, subject, body);
+        } catch (Exception e) {
         }
     }
 
     @Then("the email should be sent successfully")
     public void theEmailShouldBeSentSuccessfully() {
         assertEquals(null, exception);
-    }
-
-    @Then("an error should be thrown with message {string}")
-    public void anErrorShouldBeThrownWithMessage(String errorMessage) {
-        assertEquals(errorMessage, exception.getMessage());
     }
 }
