24733bd86f16781848e664220b4e61d7357f941e
ShahdMasri <shahd.227.almasri@gmail.com>
2024-08-24 19:15:32 +0300

    Update Userfile.java

--- src/main/java/sweetSys/Userfile.java
+++ src/main/java/sweetSys/Userfile.java
@@ -17,7 +17,7 @@ public class Userfile {
     private static final Logger LOGGER = Logger.getLogger(Userfile.class.getName());
 
 
-    private public Userfile() {
+     public Userfile() {
         //constr
     }
     public static boolean emailIsRegisted (String email){
