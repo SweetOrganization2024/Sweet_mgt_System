a3cff05e276b3918556f07d258f8a0f70a4d11b6
SamiaAsmar <175207878+SamiaAsmar@users.noreply.github.com>
2024-08-24 16:42:44 +0300

    Update Main.java

--- src/main/java/Sweet/Main.java
+++ src/main/java/Sweet/Main.java
@@ -175,7 +175,6 @@ public class Main {
     public static void menu(String type , String email) throws MessagingException {
         Scanner scanner = new Scanner(System.in);
         sweet.getInstance();
-
         boolean is_add;
         String idsweet = null;
         String namesweet = null;
@@ -225,7 +224,7 @@ public class Main {
                                 idsweet = scanner.nextLine();
                                 is_add = newSweet.isAdd(idsweet);
                                 if (is_add) {
-                                    sweet.deletesweet1(idsweet);
+                                    deletesweet1(idsweet);
                                     System.out.println("Successful delete.");
                                     saveSweetsToFile(sweet.getListOfSweet(), FILE_NAME);
                                     notificationService.notifyOwnerOfDeletedSweet(email, idsweet);
@@ -1026,4 +1025,18 @@ public class Main {
         String body = String.format("Dear Admin, a new account has been created with the following details: Name - %s, Email - %s. Please review and approve if necessary.", newAccountName, newAccountEmail);
         EmailSender.sendEmail(adminEmail, subject, body);
     }
+
+    public static void deletesweet1(String ss) {
+        List<newSweet> updatedList = new ArrayList<>();
+        for (newSweet s : sweetSys.sweet.getListOfSweet()) {
+            if (!s.getId().equals(ss)) {
+                updatedList.add(s);
+            }
+        }
+       setListOfSweet((ArrayList<newSweet>) updatedList);
+    }
+    public static void setListOfSweet(ArrayList<newSweet> listOfSweet) {
+        sweet.listOfSweet = listOfSweet;
+    }
+
 }
